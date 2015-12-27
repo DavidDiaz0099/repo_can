e7d185e321c37b745287f2575e7fe9ebf931a5c6
DavidDiaz0099 <d_r_acodavid@hotmail.com>
2015-12-26 20:41:52 -0600

    control de opto

---
+++ control steppermotor/ver.4 control de opto/stepper_motor_control/FLASH/Sources/pb_down_c.d
@@ -0,0 +1,18 @@
+Sources/pb_down_c.obj: C:\Users\HP\ 240\workspace\stepper_motor_control\Sources\pb_down.c \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\lib_ewl.prefix \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\common.prefix \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\ewl_c_version.h \
+	C:\Users\HP\ 240\workspace\stepper_motor_control\Prefix\MPC5606B_FLASH_VLE.prefix \
+	C:\Users\HP\ 240\workspace\stepper_motor_control\Project_Headers\led.h \
+	C:\Users\HP\ 240\workspace\stepper_motor_control\Project_Headers\pb.h \
+	C:\Users\HP\ 240\workspace\stepper_motor_control\Project_Headers\pb_down.h \
+	C:\Users\HP\ 240\workspace\stepper_motor_control\Project_Headers\MPC5606B.h \
+	C:\Users\HP\ 240\workspace\stepper_motor_control\Project_Headers\typedefs.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\stdint.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\ansi_parms.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\ewlGlobals.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\pa\ansi_prefix.PA_EABI.bare.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\os_enum.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\cstdint \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\limits_api.h \
+	C:\Freescale\CW\ MCU\ v10.6.4\MCU\PA_Support\ewl\EWL_C\include\ewl_lib_ext1.h 
---
+++ control steppermotor/ver.4 control de opto/stepper_motor_control/Project_Headers/STM.h
@@ -0,0 +1,15 @@
+/*
+ * STM.h
+ *
+ *  Created on: Dec 25, 2015
+ *      Author: HP 240
+ */
+
+#ifndef STM_H_
+#define STM_H_
+
+
+extern void Config_clock_STM();
+
+
+#endif /* STM_H_ */
